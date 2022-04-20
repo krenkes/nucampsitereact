e5244f70608628187d4418aa9a346454c4d4d31c
Kevin Renkes <kevinrenkes@gmail.com>
2022-04-20 09:05:10 -0500

    Controlled Form and Controlled Form Validation

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -37,14 +37,14 @@ class Main extends Component {
             );
         };
 
-        const CampsiteWithId = ({match}) => {
+        const CampsiteWithId = ({ match }) => {
             return (
-                <CampsiteInfo 
+                <CampsiteInfo
                     campsite={this.state.campsites.filter(campsite => campsite.id === +match.params.campsiteId)[0]}
                     comments={this.state.comments.filter(comment => comment.campsiteId === +match.params.campsiteId)}
                 />
             );
-        };    
+        };
 
         return (
             <div>
